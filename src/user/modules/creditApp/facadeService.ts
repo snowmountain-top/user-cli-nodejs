d5dab83859a031387086886f30817ff5dab659db
yuhan <yuhan@yunquna.com>
2024-02-22 17:22:22 +0800

    提交

--- src/user/modules/creditApp/facadeService.ts
+++ src/user/modules/creditApp/facadeService.ts
@@ -20,8 +20,8 @@ class CreditFacadeService extends BaseService implements Service.FacadeControlle
   getCommonlyTouristByUnionId(request: any): Promise<any> {
     return callApi<Service.FacadeController['getCommonlyTouristByUnionId']>(this.getApiUrl(this.getCommonlyTouristByUnionId), request)
   }
-  userLoginCommunity(request: any): Promise<any> {
-    return callApi<Service.FacadeController['userLoginCommunity']>(this.getApiUrl(this.userLoginCommunity), request)
+  userLogin(request: any): Promise<any> {
+    return callApi<Service.FacadeController['userLogin']>(this.getApiUrl(this.userLogin), request)
   }
 
 }
