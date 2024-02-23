11ee9d06b55633fc9e9afb790c2827cf172818a8
yuhan <yuhan@yunquna.com>
2024-02-23 19:02:29 +0800

    提交

--- src/user/modules/creditApp/types.ts
+++ src/user/modules/creditApp/types.ts
@@ -40,7 +40,7 @@ export namespace Service {
 
     export interface GetCreditReduceParam {
       /** 用户id */
-      unionIds: string[]
+      unionIds: string
     }
 
     export interface GrantPrepareCreditParam {
