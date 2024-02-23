2dee62d65e514115374463f8106d4005cd6c4187
zmlllll5 <zmlllll5@163.com>
2024-02-23 15:38:17 +0800

    fix

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,20 @@
 import { USER } from './types'
+import creditService from './user/modules/creditApp/creditService'
+import facadeService from './user/modules/creditApp/facadeService'
+import userBdService from './user/modules/creditApp/userBdService'
+import userService from './user/modules/creditApp/userService'
+import BizError from './errors/BizError'
 
-
+/** 业务模块 */
 export {
-  /** 业务模块 */
-  USER
+  USER,
+  creditService,
+  facadeService,
+  userBdService,
+  userService,
+  BizError
+
 }
+
+/** 枚举类 */
+export declare var UserConstants: typeof USER.Constants;
