0dcfc1e9b617d874a7f983d2c320bd2754319884
zmlllll5 <zmlllll5@163.com>
2024-02-27 12:09:33 +0800

    fix

--- src/user/modules/creditApp/userService.ts
+++ src/user/modules/creditApp/userService.ts
@@ -3,21 +3,9 @@ import BaseService from '../service'
 import {Service} from "./types";
 
 class UserService extends BaseService implements Service.UserController {
-  // queryBDRelationInfo(request: Service.Request.QueryBDRelationInfoParam): Promise<Service.Response.UserBDRelationDTO[]> {
-  //   return callApi<Service.UserController['queryBDRelationInfo']>(this.getApiUrl(this.queryBDRelationInfo), request)
-  // }
-  // sendWillExpireCreditSms(request: any): Promise<boolean> {
-  //   return callApi<Service.UserController['sendWillExpireCreditSms']>(this.getApiUrl(this.sendWillExpireCreditSms), request)
-  // }
-  // addBDBasicInfo(request: Service.Request.IBusinessDevelopmentStaff): Promise<void> {
-  //   return callApi<Service.UserController['addBDBasicInfo']>(this.getApiUrl(this.addBDBasicInfo), request)
-  // }
-  // updateBDBasicInfo(request: Service.Request.IBusinessDevelopmentStaff): Promise<void> {
-  //   return callApi<Service.UserController['updateBDBasicInfo']>(this.getApiUrl(this.updateBDBasicInfo), request)
-  // }
-  // getBdVipGroupId(request: Service.Request.getBdVipGroupIdParam): Promise<Service.Response.bdVipGroupIdDTO> {
-  //   return callApi<Service.UserController['getBdVipGroupId']>(this.getApiUrl(this.getBdVipGroupId), request)
-  // }
+  queryUserByOpenId(openId: string): Promise<any> {
+    return callApi<Service.UserController['queryUserByOpenId']>(this.getApiUrl(this.queryUserByOpenId), openId)
+  }
   protected prefixUrl: string = '/user'
 
   listUserBasicInfo(request: any): Promise<any> {
